5a65dbd2b8955aa5e9125dbfb1d0a1de139e7f19
Dima9119708 <59973263+Dima9119708@users.noreply.github.com>
2020-10-20 12:34:28 +0300

    Updates

---
+++ precache-manifest.71326d56b2a396e4261b6f67aa784d6c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ffad3b87ea6fcdff2ba4b35ea1e74d0",
+    "url": "/abzAgency/index.html"
+  },
+  {
+    "revision": "1886c21e235db5ea719c",
+    "url": "/abzAgency/static/css/main.2e93887d.chunk.css"
+  },
+  {
+    "revision": "18450e0f36086e934fc4",
+    "url": "/abzAgency/static/js/2.045bdefb.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/abzAgency/static/js/2.045bdefb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1886c21e235db5ea719c",
+    "url": "/abzAgency/static/js/main.d578bc35.chunk.js"
+  },
+  {
+    "revision": "4c330bd1046a11aa157b",
+    "url": "/abzAgency/static/js/runtime-main.9f858b32.js"
+  },
+  {
+    "revision": "71b6ce8c974d4a0abb45c9a1a9f7c405",
+    "url": "/abzAgency/static/media/banner-photo.71b6ce8c.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/abzAgency/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/abzAgency/static/media/man-laptop-v1.3cdf7897.svg"
+  },
+  {
+    "revision": "6f6bef288009ad20ca333f0cd61608c6",
+    "url": "/abzAgency/static/media/menu icon.6f6bef28.svg"
+  },
+  {
+    "revision": "3a41f082d05e2a2756c67f67f21b2324",
+    "url": "/abzAgency/static/media/photo-cover.3a41f082.svg"
+  }
+]);
